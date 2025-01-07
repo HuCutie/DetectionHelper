5f7025920fb5bd53446b3692511baff252633069
HuCutie <husmallcutie@gmail.com>
2025-01-07 17:44:47 +0800

    yolo2 voc/coco user convenience adjustment

--- yolo2coco.py
+++ yolo2coco.py
@@ -2,8 +2,8 @@ import argparse
 import json
 import os
 from datetime import datetime
-
 import cv2
+from tqdm import tqdm
 
 coco = dict()
 coco['images'] = []
@@ -14,117 +14,112 @@ coco['categories'] = []
 category_set = dict()
 image_set = set()
 
-image_id = 000000
+image_id = 0
 annotation_id = 0
 
 def addCatItem(category_dict):
+    """Add category items to the coco dictionary"""
     for k, v in category_dict.items():
-        category_item = dict()
-        category_item['supercategory'] = 'none'
-        category_item['id'] = int(k)
-        category_item['name'] = v
+        category_item = {
+            'supercategory': 'none',
+            'id': int(k),
+            'name': v
+        }
         coco['categories'].append(category_item)
 
 def addImgItem(file_name, size):
+    """Add image item to the coco dictionary"""
     global image_id
     image_id += 1
-    image_item = dict()
-    image_item['id'] = image_id
-    image_item['file_name'] = file_name
-    image_item['width'] = size[1]
-    image_item['height'] = size[0]
-    image_item['license'] = None
-    image_item['flickr_url'] = None
-    image_item['coco_url'] = None
-    image_item['date_captured'] = str(datetime.today())
+    image_item = {
+        'id': image_id,
+        'file_name': file_name,
+        'width': size[1],
+        'height': size[0],
+        'license': None,
+        'flickr_url': None,
+        'coco_url': None,
+        'date_captured': str(datetime.today())
+    }
     coco['images'].append(image_item)
     image_set.add(file_name)
     return image_id
 
 def addAnnoItem(object_name, image_id, category_id, bbox):
+    """Add annotation item to the coco dictionary"""
     global annotation_id
-    annotation_item = dict()
-    annotation_item['segmentation'] = []
-    seg = []
-    # bbox[] is x,y,w,h
-    # left_top
-    seg.append(bbox[0])
-    seg.append(bbox[1])
-    # left_bottom
-    seg.append(bbox[0])
-    seg.append(bbox[1] + bbox[3])
-    # right_bottom
-    seg.append(bbox[0] + bbox[2])
-    seg.append(bbox[1] + bbox[3])
-    # right_top
-    seg.append(bbox[0] + bbox[2])
-    seg.append(bbox[1])
-
-    annotation_item['segmentation'].append(seg)
-
-    annotation_item['area'] = bbox[2] * bbox[3]
-    annotation_item['iscrowd'] = 0
-    annotation_item['ignore'] = 0
-    annotation_item['image_id'] = image_id
-    annotation_item['bbox'] = bbox
-    annotation_item['category_id'] = category_id
+    annotation_item = {
+        'segmentation': [[
+            bbox[0], bbox[1], # left_top
+            bbox[0], bbox[1] + bbox[3], # left_bottom
+            bbox[0] + bbox[2], bbox[1] + bbox[3], # right_bottom
+            bbox[0] + bbox[2], bbox[1] # right_top
+        ]],
+        'area': bbox[2] * bbox[3],
+        'iscrowd': 0,
+        'ignore': 0,
+        'image_id': image_id,
+        'bbox': bbox,
+        'category_id': category_id
+    }
     annotation_id += 1
     annotation_item['id'] = annotation_id
     coco['annotations'].append(annotation_item)
 
 def xywhn2xywh(bbox, size):
+    """Convert normalized coordinates to absolute coordinates"""
     bbox = list(map(float, bbox))
     size = list(map(float, size))
     xmin = (bbox[0] - bbox[2] / 2.) * size[1]
     ymin = (bbox[1] - bbox[3] / 2.) * size[0]
     w = bbox[2] * size[1]
     h = bbox[3] * size[0]
-    box = (xmin, ymin, w, h)
-    return list(map(int, box))
+    return list(map(int, (xmin, ymin, w, h)))
 
 def parse(anno_path, save_path, image_path):
-    assert os.path.exists(image_path), "ERROR {} dose not exists".format(image_path)
-    assert os.path.exists(anno_path), "ERROR {} dose not exists".format(anno_path)
+    """Parse YOLO annotations and convert to COCO format"""
+    assert os.path.exists(image_path), f"ERROR: {image_path} does not exist"
+    assert os.path.exists(anno_path), f"ERROR: {anno_path} does not exist"
 
-    category_set = []
-    with open(anno_path + '/classes.txt', 'r') as f:
-        for i in f.readlines():
-            category_set.append(i.strip())
-    category_id = dict((k, v) for k, v in enumerate(category_set))
-    addCatItem(category_id)
+    # Read category names
+    with open(os.path.join(anno_path, 'classes.txt'), 'r') as f:
+        category_set = {k: v.strip() for k, v in enumerate(f.readlines())}
+    addCatItem(category_set)
 
-    images = [os.path.join(image_path, i) for i in os.listdir(image_path)]
-    files = [os.path.join(anno_path, i) for i in os.listdir(anno_path)]
-    images_index = dict((v.split(os.sep)[-1][:-4], k) for k, v in enumerate(images))
-    for file in files:
-        if os.path.splitext(file)[-1] != '.txt' or 'classes' in file.split(os.sep)[-1]:
-            continue
-        if file.split(os.sep)[-1][:-4] in images_index:
-            index = images_index[file.split(os.sep)[-1][:-4]]
-            img = cv2.imread(images[index])
+    # Get all image and annotation files
+    images = {os.path.splitext(i)[0]: os.path.join(image_path, i) for i in os.listdir(image_path)}
+    files = [os.path.join(anno_path, i) for i in os.listdir(anno_path) if i.endswith('.txt')]
+
+    # Use tqdm for progress bar when processing annotation files
+    for file in tqdm(files, desc="Processing annotation files", ncols=100):
+        filename = os.path.splitext(os.path.basename(file))[0]
+        if filename in images:
+            img = cv2.imread(images[filename])
             shape = img.shape
-            filename = images[index].split(os.sep)[-1]
-            current_image_id = addImgItem(filename, shape)
+            current_image_id = addImgItem(os.path.basename(images[filename]), shape)
         else:
             continue
+        
         with open(file, 'r') as fid:
-            for i in fid.readlines():
-                i = i.strip().split()
-                category = int(i[0])
-                category_name = category_id[category]
-                bbox = xywhn2xywh((i[1], i[2], i[3], i[4]), shape)
-                addAnnoItem(category_name, current_image_id, category, bbox)
+            for line in fid.readlines():
+                category, x_center, y_center, w, h = map(float, line.strip().split())
+                category_id = int(category)
+                category_name = category_set[category_id]
+                bbox = xywhn2xywh((x_center, y_center, w, h), shape)
+                addAnnoItem(category_name, current_image_id, category_id, bbox)
 
-    json.dump(coco, open(save_path, 'w'))
-    print("class nums:{}".format(len(coco['categories'])))
-    print("image nums:{}".format(len(coco['images'])))
-    print("bbox nums:{}".format(len(coco['annotations'])))
+    # Save COCO format data
+    with open(save_path, 'w') as json_file:
+        json.dump(coco, json_file)
+    print(f"class nums: {len(coco['categories'])}")
+    print(f"image nums: {len(coco['images'])}")
+    print(f"bbox nums: {len(coco['annotations'])}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ap', '--anno-path', type=str, default='/workspace/0test/labelsnew/yolo/voc2yolo', help='yolo .txt path')
-    parser.add_argument('-sp', '--save-path', type=str, default='/workspace/0test/labelsnew/coco/yolo2coco.json', help='coco .json save path')
-    parser.add_argument('-ip', '--img-path', type=str, default='/workspace/0test/images', help='yolo images path')
+    parser.add_argument('-ap', '--anno-path', type=str, required=True, help='Path to YOLO annotation folder')
+    parser.add_argument('-sp', '--save-path', type=str, required=True, help='Path to save the generated COCO JSON file')
+    parser.add_argument('-ip', '--img-path', type=str, required=True, help='Path to YOLO images folder')
     opt = parser.parse_args()
 
     print(opt)
--- yolo2voc.py
+++ yolo2voc.py
@@ -1,15 +1,16 @@
 import argparse
 import os
-import shutil
 import cv2
 from lxml import etree, objectify
 from tqdm import tqdm
 
+# Global variables
 images_nums = 0
 category_nums = 0
 bbox_nums = 0
 
 def save_anno_to_xml(filename, size, objs, save_path):
+    """Save the annotation information to XML format"""
     E = objectify.ElementMaker(annotate=False)
     anno_tree = E.annotation(
         E.folder("DATA"),
@@ -27,82 +28,100 @@ def save_anno_to_xml(filename, size, objs, save_path):
         E.segmented(0)
     )
     for obj in objs:
-        E2 = objectify.ElementMaker(annotate=False)
-        anno_tree2 = E2.object(
-            E.name(obj[0]),
-            E.pose("Unspecified"),
-            E.truncated(0),
-            E.difficult(0),
-            E.bndbox(
-                E.xmin(obj[1][0]),
-                E.ymin(obj[1][1]),
-                E.xmax(obj[1][2]),
-                E.ymax(obj[1][3])
+        anno_tree.append(
+            E.object(
+                E.name(obj[0]),
+                E.pose("Unspecified"),
+                E.truncated(0),
+                E.difficult(0),
+                E.bndbox(
+                    E.xmin(obj[1][0]),
+                    E.ymin(obj[1][1]),
+                    E.xmax(obj[1][2]),
+                    E.ymax(obj[1][3])
+                )
             )
         )
-        anno_tree.append(anno_tree2)
     anno_path = os.path.join(save_path, filename[:-3] + "xml")
     etree.ElementTree(anno_tree).write(anno_path, pretty_print=True)
 
-
 def xywhn2xyxy(bbox, size):
+    """Convert YOLO bbox (xywhn) to (xyxy) format"""
     bbox = list(map(float, bbox))
     size = list(map(float, size))
     xmin = (bbox[0] - bbox[2] / 2.) * size[1]
     ymin = (bbox[1] - bbox[3] / 2.) * size[0]
     xmax = (bbox[0] + bbox[2] / 2.) * size[1]
     ymax = (bbox[1] + bbox[3] / 2.) * size[0]
-    box = [xmin, ymin, xmax, ymax]
-    return list(map(int, box))
-
+    return [int(xmin), int(ymin), int(xmax), int(ymax)]
 
 def parse(anno_path, save_path, image_path):
+    """Parse YOLO annotation files and save to VOC XML format"""
     global images_nums, category_nums, bbox_nums
-    assert os.path.exists(image_path), "ERROR {} dose not exists".format(image_path)
-    assert os.path.exists(anno_path), "ERROR {} dose not exists".format(anno_path)
+    
+    # Check if the provided paths exist
+    assert os.path.exists(image_path), f"ERROR: {image_path} does not exist"
+    assert os.path.exists(anno_path), f"ERROR: {anno_path} does not exist"
+    
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
+    # Read category names
     category_set = []
-    with open(anno_path + '/classes.txt', 'r') as f:
-        for i in f.readlines():
-            category_set.append(i.strip())
+    with open(os.path.join(anno_path, 'classes.txt'), 'r') as f:
+        category_set = [line.strip() for line in f.readlines()]
     category_nums = len(category_set)
-    category_id = dict((k, v) for k, v in enumerate(category_set))
+    category_id = {k: v for k, v in enumerate(category_set)}
 
-    images = [os.path.join(image_path, i) for i in os.listdir(image_path)]
-    files = [os.path.join(anno_path, i) for i in os.listdir(anno_path)]
-    images_index = dict((v.split(os.sep)[-1][:-4], k) for k, v in enumerate(images))
+    # Prepare image and annotation file lists
+    images = [os.path.join(image_path, img) for img in os.listdir(image_path)]
+    image_index = {os.path.splitext(os.path.basename(img))[0]: img for img in images}
+    files = [os.path.join(anno_path, f) for f in os.listdir(anno_path) if f.endswith('.txt')]
+
     images_nums = len(images)
 
-    for file in tqdm(files):
-        if os.path.splitext(file)[-1] != '.txt' or 'classes' in file.split(os.sep)[-1]:
+    # Iterate through each annotation file with a progress bar
+    for file in tqdm(files, desc="Processing annotations", ncols=100):
+        filename = os.path.splitext(os.path.basename(file))[0]
+        
+        # Skip if file is not an annotation file or is a class file
+        if 'classes' in filename or not file.endswith('.txt'):
             continue
-        if file.split(os.sep)[-1][:-4] in images_index:
-            index = images_index[file.split(os.sep)[-1][:-4]]
-            img = cv2.imread(images[index])
-            shape = img.shape
-            filename = images[index].split(os.sep)[-1]
+
+        # Find corresponding image
+        if filename in image_index:
+            img_path = image_index[filename]
+            img = cv2.imread(img_path)
+            shape = img.shape  # Get image shape (height, width, channels)
+        
         else:
             continue
+        
         objects = []
         with open(file, 'r') as fid:
-            for i in fid.readlines():
-                i = i.strip().split()
-                category = int(i[0])
+            for line in fid.readlines():
+                # Read and process each line (object information)
+                parts = line.strip().split()
+                category = int(parts[0])
                 category_name = category_id[category]
-                bbox = xywhn2xyxy((i[1], i[2], i[3], i[4]), shape)
-                obj = [category_name, bbox]
-                objects.append(obj)
+                bbox = xywhn2xyxy(parts[1:], shape)
+                objects.append([category_name, bbox])
+
+        # Update bbox count and save annotations
         bbox_nums += len(objects)
         save_anno_to_xml(filename, shape, objects, save_path)
 
+    # Print final statistics
+    print(f'class nums: {category_nums}')
+    print(f'image nums: {images_nums}')
+    print(f'bbox nums: {bbox_nums}')
 
 if __name__ == '__main__':
+    # Argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ap', '--anno-path', type=str, default='/workspace/0test/labelsnew/yolo/coco2yolo', help='yolo .txt path')
-    parser.add_argument('-sp', '--save-path', type=str, default='/workspace/0test/labelsnew/voc/yolo2voc', help='voc .xml save path')
-    parser.add_argument('-ip', '--img-path', type=str, default='/workspace/0test/images', help='yolo images path')
+    parser.add_argument('-ap', '--anno-path', type=str, default='/workspace/0test/labelsnew/yolo/coco2yolo', help='Path to YOLO .txt annotations')
+    parser.add_argument('-sp', '--save-path', type=str, default='/workspace/0test/labelsnew/voc/yolo2voc', help='Path to save VOC .xml annotations')
+    parser.add_argument('-ip', '--img-path', type=str, default='/workspace/0test/images', help='Path to YOLO images')
     opt = parser.parse_args()
 
     print(opt)
